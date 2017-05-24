a3767a3110bab2043e52889f2524e1d6d1877b0f
Matthew Connelly <matthew.connelly9@gmail.com>
2017-05-24 14:39:47 -0400

    models defined with associations

--- db/models/campus.js
+++ db/models/campus.js
@@ -3,6 +3,8 @@ var Sequelize = require('sequelize')
 var db = require('../index.js')
 
 
-module.exports = db.define('user', {
+
+module.exports = db.define('campus', {
   name: Sequelize.STRING,
-})
+  imageURL: Sequelize.STRING,
+  })
